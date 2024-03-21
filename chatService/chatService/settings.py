c9de0274c631263a3134ec20371a7d87116d6a14
jobayer-rahman <jobayer_rahman@yahoo.com>
2024-03-22 01:11:30 +0600

    Configured settings for mysql database connectivity.

--- chatService/chatService/settings.py
+++ chatService/chatService/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'chatService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'db',
+        'PASSWORD': 'db',
+        'HOST': 'db',
+        # 'PORT': '36060',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
